0fdebabc129878a9743ae70e397b80bda90cfb87
Jason Laurich <jlaurich@uoguelph.ca>
2024-11-28 09:12:40 -0500

    Added scripts to transform and analyze plate reader data from dilution experiments
    CSV from plate reader needs to be transformed to be legible (01), then we need to look at relationship between Chlamy concentration, as well as Ensifer meliloti 1021 and 1022, to (for now) fit OD curves for bacterial abundance and identify the lowest cell concentrations detectable by our plate reader.

---
+++ pilot-projects/01_dilution_platereader_data_transformation.R
@@ -0,0 +1,11 @@
+# Jason R Laurich
+# November 28, 2024
+
+# Script to process plate reader data for dilution experimental pilot
+# Save readable, interpretable csv file
+
+############### Access the csv file ######################
+
+
+
+############### Process and transform data #####################
---
+++ pilot-projects/02_dilution_data_exploration.R
@@ -0,0 +1,14 @@
+# Jason R Laurich
+# November 28, 2024
+
+# Script to analyse pilot data from the dilution experiments.
+# For now, want to initially explore the relationship between Ensifer dilution and OD readings (600, 700, 750)
+# Get a sense of the minimum detection threshold of our plate reader. 
+
+############## Get and examine the data #######################
+
+
+############## Exploration and plotting #######################
+
+
+############# More sophisticated model exploration #####################

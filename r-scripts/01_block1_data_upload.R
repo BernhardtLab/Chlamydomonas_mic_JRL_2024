f45174dd153a9436cfb80324b5adfc68be9c6f12
Jason Laurich <jlaurich@uoguelph.ca>
2025-05-12 12:51:30 -0400

    Updated 01_block1_data...R
    Caught an error in data uploading, fixing now.

--- r-scripts/01_block1_data_upload.R
+++ r-scripts/01_block1_data_upload.R
@@ -12,6 +12,7 @@ library(readxl)
 library(lubridate)
 library(openxlsx)
 library(cowplot)
+library(dplyr)
 
 # Script to upload and organize raw data --------------------------------
 
@@ -36,10 +37,10 @@ for (p in 1:2){ # Plates
     file.path <- paste("raw-data/JRL_block1_", t, "_", p, "_", i, ".xlsx", sep = "")
     # t is the temp, p is the plate, i is the read
     
-    df.raw <- read_excel(file.path, range = "C48:N64")
+    df.raw <- read_excel(file.path, range = "D50:M62")
     
     df.raw <- as.data.frame(df.raw)
-    df.raw$meas <- rep(c("RFU", "OD600"),8)
+    df.raw$meas <- rep(c("RFU", "OD600"),6)
     
     RFU <- numeric()
     OD600 <- numeric()
@@ -53,7 +54,7 @@ for (p in 1:2){ # Plates
       
       x <- ceiling(j/2) # This function round all values above an integer to the next. I'll use this to only record one line for each row of wells.
       
-      for (c in 1:12){
+      for (c in 1:10){
         
         if (j == 2*x){ # Only record row and column every two rows.
           Row <- c(Row, x) # row number
@@ -98,7 +99,7 @@ for (p in 1:2){ # Plates
     df.plt$Column <- as.integer(df.plt$Column)
     
     df.plt <- df.plt %>% 
-      mutate(Well.at.T = (Plate-1)*96 + (Row-1)*12 + Column)
+      mutate(Well.at.T = (Plate-1)*60 + (Row-1)*10 + Column)
     
     df.proc <- df.plt %>%
       left_join(df.design.temp.plate, by = "Well.at.T") # This is the full data we want for each frame (don't need elapsed time)
@@ -902,6 +903,9 @@ p30 <- ggplot(df.30, aes(x = days, y = RFU, group = Well.at.T)) +
 
 p30
 
+
+# Data exploration with plots ---------------------------------------------
+
 p.comp <- plot_grid(p8, p14, p20, p25, p30, p33, p35, p39, p43,
                     ncol = 5)
 
@@ -911,4 +915,270 @@ df.blk1 <- rbind(df.8, df.14, df.20, df.25, df.30, df.33, df.35, df.39, df.43)
 
 write.csv(df.blk1, "processed-data/01_blk1_rawdata.csv") # Save raw data. This still needs some accounting (e.g. errors in the notes column)
 
+head(df.blk1)
+str(df.blk1)
 
+df.blk1$Microbe <- as.factor(df.blk1$Microbe)
+levels(df.blk1$Microbe) # Right so we have BLANK in there as an extra factor level. That's fine.
+
+df.blk1 <- df.blk1 %>%
+  filter(Microbe != 'BLANK') %>%
+  droplevels() %>%
+  mutate(Microbe = fct_relevel(Microbe, 
+                               "none", "1", "2", "3", "4", "5", "6", "7", "8", 
+                               "9", "10", "11", "12", "13", "14", "15", "all"))
+
+p8.2 <- ggplot(df.blk1[df.blk1$Temperature.C == 8, ], 
+               aes(x = days, y = RFU, group = Well.at.T, colour = Microbe)) +
+  geom_line(alpha = 0.9, linewidth = 1) +
+  theme_classic() +
+  labs(title = "8 C") +
+  scale_color_manual(
+    name = "Microbial treatment",  # Update the legend title
+    values = c("none" = "black",
+               "1" = "mediumorchid4",
+               "2" = "magenta2",
+               "3" = "tan",
+               "4" = "turquoise1",
+               "5" = "dodgerblue",
+               "6" = "royalblue4",  
+               "7" = "blue",
+               "8" = "deeppink",
+               "9" = "darkorange2",
+               "10" = "orange",
+               "11" = "firebrick",
+               "12" = "orangered2",
+               "13" = "red1",  
+               "14" = "forestgreen",
+               "15" = "darkolivegreen2",
+               "all" = "goldenrod1")
+  )
+  
+p8.2
+
+p14.2 <- ggplot(df.blk1[df.blk1$Temperature.C == 14, ], 
+               aes(x = days, y = RFU, group = Well.at.T, colour = Microbe)) +
+  geom_line(alpha = 0.9, linewidth = 1) +
+  theme_classic() +
+  labs(title = "14 C") +
+  scale_color_manual(
+    name = "Microbial treatment",  # Update the legend title
+    values = c("none" = "black",
+               "1" = "mediumorchid4",
+               "2" = "magenta2",
+               "3" = "tan",
+               "4" = "turquoise1",
+               "5" = "dodgerblue",
+               "6" = "royalblue4",  
+               "7" = "blue",
+               "8" = "deeppink",
+               "9" = "darkorange2",
+               "10" = "orange",
+               "11" = "firebrick",
+               "12" = "orangered2",
+               "13" = "red1",  
+               "14" = "forestgreen",
+               "15" = "darkolivegreen2",
+               "all" = "goldenrod1")
+  )
+
+p14.2
+
+p20.2 <- ggplot(df.blk1[df.blk1$Temperature.C == 20, ], 
+               aes(x = days, y = RFU, group = Well.at.T, colour = Microbe)) +
+  geom_line(alpha = 0.9, linewidth = 1) +
+  theme_classic() +
+  labs(title = "20 C") +
+  scale_color_manual(
+    name = "Microbial treatment",  # Update the legend title
+    values = c("none" = "black",
+               "1" = "mediumorchid4",
+               "2" = "magenta2",
+               "3" = "tan",
+               "4" = "turquoise1",
+               "5" = "dodgerblue",
+               "6" = "royalblue4",  
+               "7" = "blue",
+               "8" = "deeppink",
+               "9" = "darkorange2",
+               "10" = "orange",
+               "11" = "firebrick",
+               "12" = "orangered2",
+               "13" = "red1",  
+               "14" = "forestgreen",
+               "15" = "darkolivegreen2",
+               "all" = "goldenrod1")
+  )
+
+p20.2
+
+p25.2 <- ggplot(df.blk1[df.blk1$Temperature.C == 25, ], 
+                aes(x = days, y = RFU, group = Well.at.T, colour = Microbe)) +
+  geom_line(alpha = 0.9, linewidth = 1) +
+  theme_classic() +
+  labs(title = "25 C") +
+  scale_color_manual(
+    name = "Microbial treatment",  # Update the legend title
+    values = c("none" = "black",
+               "1" = "mediumorchid4",
+               "2" = "magenta2",
+               "3" = "tan",
+               "4" = "turquoise1",
+               "5" = "dodgerblue",
+               "6" = "royalblue4",  
+               "7" = "blue",
+               "8" = "deeppink",
+               "9" = "darkorange2",
+               "10" = "orange",
+               "11" = "firebrick",
+               "12" = "orangered2",
+               "13" = "red1",  
+               "14" = "forestgreen",
+               "15" = "darkolivegreen2",
+               "all" = "goldenrod1")
+  )
+
+p25.2
+
+p33.2 <- ggplot(df.blk1[df.blk1$Temperature.C == 33, ], 
+                aes(x = days, y = RFU, group = Well.at.T, colour = Microbe)) +
+  geom_line(alpha = 0.9, linewidth = 1) +
+  theme_classic() +
+  labs(title = "33 C") +
+  scale_color_manual(
+    name = "Microbial treatment",  # Update the legend title
+    values = c("none" = "black",
+               "1" = "mediumorchid4",
+               "2" = "magenta2",
+               "3" = "tan",
+               "4" = "turquoise1",
+               "5" = "dodgerblue",
+               "6" = "royalblue4",  
+               "7" = "blue",
+               "8" = "deeppink",
+               "9" = "darkorange2",
+               "10" = "orange",
+               "11" = "firebrick",
+               "12" = "orangered2",
+               "13" = "red1",  
+               "14" = "forestgreen",
+               "15" = "darkolivegreen2",
+               "all" = "goldenrod1")
+  )
+
+p33.2
+
+p35.2 <- ggplot(df.blk1[df.blk1$Temperature.C == 35, ], 
+                aes(x = days, y = RFU, group = Well.at.T, colour = Microbe)) +
+  geom_line(alpha = 0.9, linewidth = 1) +
+  theme_classic() +
+  labs(title = "35 C") +
+  scale_color_manual(
+    name = "Microbial treatment",  # Update the legend title
+    values = c("none" = "black",
+               "1" = "mediumorchid4",
+               "2" = "magenta2",
+               "3" = "tan",
+               "4" = "turquoise1",
+               "5" = "dodgerblue",
+               "6" = "royalblue4",  
+               "7" = "blue",
+               "8" = "deeppink",
+               "9" = "darkorange2",
+               "10" = "orange",
+               "11" = "firebrick",
+               "12" = "orangered2",
+               "13" = "red1",  
+               "14" = "forestgreen",
+               "15" = "darkolivegreen2",
+               "all" = "goldenrod1")
+  )
+
+p35.2
+
+p39.2 <- ggplot(df.blk1[df.blk1$Temperature.C == 39, ], 
+                aes(x = days, y = RFU, group = Well.at.T, colour = Microbe)) +
+  geom_line(alpha = 0.9, linewidth = 1) +
+  theme_classic() +
+  labs(title = "39 C") +
+  scale_color_manual(
+    name = "Microbial treatment",  # Update the legend title
+    values = c("none" = "black",
+               "1" = "mediumorchid4",
+               "2" = "magenta2",
+               "3" = "tan",
+               "4" = "turquoise1",
+               "5" = "dodgerblue",
+               "6" = "royalblue4",  
+               "7" = "blue",
+               "8" = "deeppink",
+               "9" = "darkorange2",
+               "10" = "orange",
+               "11" = "firebrick",
+               "12" = "orangered2",
+               "13" = "red1",  
+               "14" = "forestgreen",
+               "15" = "darkolivegreen2",
+               "all" = "goldenrod1")
+  )
+
+p39.2
+
+p43.2 <- ggplot(df.blk1[df.blk1$Temperature.C == 43, ], 
+                aes(x = days, y = RFU, group = Well.at.T, colour = Microbe)) +
+  geom_line(alpha = 0.9, linewidth = 1) +
+  theme_classic() +
+  labs(title = "43 C") +
+  scale_color_manual(
+    name = "Microbial treatment",  # Update the legend title
+    values = c("none" = "black",
+               "1" = "mediumorchid4",
+               "2" = "magenta2",
+               "3" = "tan",
+               "4" = "turquoise1",
+               "5" = "dodgerblue",
+               "6" = "royalblue4",  
+               "7" = "blue",
+               "8" = "deeppink",
+               "9" = "darkorange2",
+               "10" = "orange",
+               "11" = "firebrick",
+               "12" = "orangered2",
+               "13" = "red1",  
+               "14" = "forestgreen",
+               "15" = "darkolivegreen2",
+               "all" = "goldenrod1")
+  )
+
+p43.2
+
+df.blk1.t <- df.blk1 %>% 
+  filter(Nitrogen.conc.µM == 1000, Salt.conc.g.l == 0)
+
+p30.2 <- ggplot(df.blk1.t[df.blk1.t$Temperature.C == 30, ], 
+                aes(x = days, y = RFU, group = Well.at.T, colour = Microbe)) +
+  geom_line(alpha = 0.9, linewidth = 1) +
+  theme_classic() +
+  labs(title = "30 C") +
+  scale_color_manual(
+    name = "Microbial treatment",  # Update the legend title
+    values = c("none" = "black",
+               "1" = "mediumorchid4",
+               "2" = "magenta2",
+               "3" = "tan",
+               "4" = "turquoise1",
+               "5" = "dodgerblue",
+               "6" = "royalblue4",  
+               "7" = "blue",
+               "8" = "deeppink",
+               "9" = "darkorange2",
+               "10" = "orange",
+               "11" = "firebrick",
+               "12" = "orangered2",
+               "13" = "red1",  
+               "14" = "forestgreen",
+               "15" = "darkolivegreen2",
+               "all" = "goldenrod1")
+  )
+
+p30.2
